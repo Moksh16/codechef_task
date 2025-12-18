79166aa5dd3526ccee15fe71f04300a21b101b02
Mokh Kumar Shah <mokshshah2020@gmail.com>
2025-12-18 21:40:33 +0530

    code for deleting a post added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Response
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from datetime import datetime, timedelta
 from jose import JWTError,jwt
 from passlib.context import CryptContext
 from fastapi.params import Body
+from random import randrange
+
 
 oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="Login")
 password_hasher = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -13,21 +15,35 @@ class Post(BaseModel):
     argument: str
     evidence: str
 
-
-secret_key = "897fd83ed4066efcbcdabd0d204e20897b3966555d421916581435833ef6c9c2"
-algorithm = "HS256"
 app = FastAPI()
-submissions= {}
-@app.post('/auth/register')
+submissions= []
+
+
+def find_index_post(id):
+    for i,p in enumerate(submissions):
+        if p["id"] ==id:
+            return i
+
+@app.post('/auth/signup')
 def register(username):
     return {"message": f"{username}, you are registered"}
 
 
-@app.post('/argument')
+@app.post('/case/submit')
 def add_argument(post: Post):
-    return {"Message":"Argument received" }
+    my_post = post.dict()
+    my_post["key"] = randrange(0,1000000)
+    submissions.append(my_post)
+    return {"Message":"Argument and evidence received" }
 
-@app.get('/submission')
+@app.get('/case/all')
 def get_submissions():
     return submissions
 
+app.delete('/case/delete/{id}', status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id : int):
+    index = find_index_post(id)
+    if index ==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post not found")
+    submissions.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
